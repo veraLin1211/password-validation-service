8a8f2a14435489e620d794a3cda4edd5027256c8
VeraLin <verrralin@gmail.com>
2023-07-05 15:26:58 +0800

    remove validation service classes

--- src/main/java/com/example/demo/validation/PasswordValidation.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.validation;
-
-public interface PasswordValidation {
-
-    public boolean validate(String password);
-
-}
--- src/main/java/com/example/demo/validation/character/rule/evaluation/Evaluation.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.validation.character.rule.evaluation;
-
-import com.example.demo.validation.character.model.dto.CharConfig;
-
-public interface Evaluation {
-
-    boolean evaluate(String password, CharConfig config);
-
-}

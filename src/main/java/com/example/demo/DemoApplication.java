1c6e1eacf32caa1d512a6cea3ec10ea8554ea9d4
VeraLin <verrralin@gmail.com>
2023-07-05 10:47:57 +0800

    feat: put @Generated to ignore main() when generating coverage report

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
